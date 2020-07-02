c4fce0572fcb7c43f40ec489e822b4d45c500071
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-07-03 00:42:57 +0530

    fonts changed and using custom fonts in our app now

--- screenappflutter/lib/main.dart
+++ screenappflutter/lib/main.dart
@@ -13,7 +13,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.black,
+        accentColor: Colors.orange,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          title: TextStyle(
+            fontSize: 24,
+            fontFamily: 'RobotoCondensed',
+          )
+        )
       ),
       home: CatagoriesScreen(),
     );
