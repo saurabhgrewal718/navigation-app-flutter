221b18f54d9a6826312c41c68f8d75ec211063e2
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-07-03 00:21:10 +0530

    the grid view shown sucessfully on the home screen and the first screen is Catagoryscreen too

--- screenappflutter/lib/catagories_screen.dart
+++ screenappflutter/lib/catagories_screen.dart
@@ -5,14 +5,20 @@ import './dummy_data.dart';
 class CatagoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals App'),
+      ),
+      body: GridView(
+      padding: EdgeInsets.all(20),
       children: DUMMY_CATEGORIES
       .map((catData) => CategoryItem(
         catData.title, 
         catData.color
        ),
-      ),
+      ).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200,childAspectRatio: 3/2,crossAxisSpacing: 20,mainAxisSpacing: 20),
+    ),
     );
   }
 }
