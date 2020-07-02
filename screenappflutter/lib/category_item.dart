6c9f84364b45593253a4d53e9777287bd3c482d5
Saurabh Grewal <saurabhgrewal718@gmail.com>
2020-07-03 01:25:31 +0530

    new screen appers on tap

--- screenappflutter/lib/category_item.dart
+++ screenappflutter/lib/category_item.dart
@@ -1,33 +1,49 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryItem(this.title,this.color);
+  
+  void selectCatagory(BuildContext ctx){
+    Navigator.of(ctx).push(MaterialPageRoute(builder:(_){
+      return CategoryMealsScreen();
+     },
+    ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: TextStyle(
-          fontSize: 20,
-            fontFamily: 'RobotoCondensed',
-            fontWeight: FontWeight.bold
+    return InkWell(
+        onTap: () => selectCatagory(context),
+        splashColor: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(15),
+          child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold
+          ),
         ),
+        decoration: BoxDecoration(gradient: LinearGradient(colors: [
+          color.withOpacity(0.7),
+          color,
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
       ),
-      decoration: BoxDecoration(gradient: LinearGradient(colors: [
-        color.withOpacity(0.7),
-        color,
-      ],
-      begin: Alignment.topLeft,
-      end: Alignment.bottomRight,
-    ),
-    borderRadius: BorderRadius.circular(15),
-    ),
-  );
+      borderRadius: BorderRadius.circular(15),
+      ),
+  ),
+    );
   }
 }
---
+++ screenappflutter/lib/category_meals_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Meals Screen"),),
+      body:Center(
+      child: Text("this is the next page")
+      ),
+    );
+  }
+}
